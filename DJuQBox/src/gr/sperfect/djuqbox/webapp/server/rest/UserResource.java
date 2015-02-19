647887ce5d1d9e73fad8abdb9afa8dbe8a4df232
sperfect <sperfect@gmail.com>
2015-02-19 18:28:43 +0200

    UserResource

---
+++ DJuQBox/src/gr/sperfect/djuqbox/webapp/server/rest/UserResource.java
@@ -0,0 +1,32 @@
+package gr.sperfect.djuqbox.webapp.server.rest;
+
+import gr.sperfect.djuqbox.webapp.shared.data.Room;
+import gr.sperfect.djuqbox.webapp.shared.data.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+
+@Path("/user")
+public class UserResource {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)	
+	public List<User> getUsers(@Context HttpHeaders headers) {
+		
+		List<User>  ret = new ArrayList<User>();
+		ret.add(new User("room0"));
+		ret.add(new User("room1"));
+		
+
+
+		return ret; 
+	}
+	
+}
