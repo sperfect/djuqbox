ff7d9bbeb9ce7e6b89e07bfe5b1429d7658223a5
sperfect <sperfect@gmail.com>
2015-02-21 00:59:09 +0200

    playlist song

---
+++ DJuQBox/src/gr/sperfect/djuqbox/webapp/shared/data/Playlist.java
@@ -0,0 +1,22 @@
+package gr.sperfect.djuqbox.webapp.shared.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Playlist extends BaseDataClass {
+
+	public Playlist() {
+
+	}
+
+	
+	List<Song> songs = new ArrayList<Song>();
+
+
+
+	// 8a mpei kati tetoio?
+	// public void addSong() {
+	//
+	// }
+
+}
---
+++ DJuQBox/src/gr/sperfect/djuqbox/webapp/shared/data/Song.java
@@ -0,0 +1,10 @@
+package gr.sperfect.djuqbox.webapp.shared.data;
+
+public class Song extends BaseDataClass {
+
+	public Song() {
+	
+	}
+	
+	public String url;
+}
