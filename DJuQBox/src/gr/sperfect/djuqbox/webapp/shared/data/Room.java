016bc069ceaca770c28ee5ba98a33de8b961bd36
sperfect <sperfect@gmail.com>
2015-02-21 01:26:25 +0200

    //xtypaei ston client? @Entity sto Room

--- DJuQBox/src/gr/sperfect/djuqbox/webapp/shared/data/Room.java
+++ DJuQBox/src/gr/sperfect/djuqbox/webapp/shared/data/Room.java
@@ -1,5 +1,10 @@
 package gr.sperfect.djuqbox.webapp.shared.data;
 
+import com.googlecode.objectify.annotation.Entity;
+
+
+//test xtypaei ston client?
+@Entity
 public class Room extends BaseDataClass {
 
 	
