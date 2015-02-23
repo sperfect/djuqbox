62a952adc89d800f816d95e66244bda75dde9ded
sperfect petralona <sperfect@gmail.com>
2015-02-23 23:14:59 +0200

    cleanup example source

--- DJuQBox/src/gr/sperfect/djuqbox/webapp/client/DJuQBox.java
+++ DJuQBox/src/gr/sperfect/djuqbox/webapp/client/DJuQBox.java
@@ -38,11 +38,9 @@ public class DJuQBox implements EntryPoint {
 	private static final String SERVER_ERROR = "An error occurred while "
 			+ "attempting to contact the server. Please check your network " + "connection and try again.";
 
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting
-	 * service.
-	 */
-	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
+	
+	
+	
 
 	//http://blog.javaforge.net/post/30469901979/gwt-rest
 	static {
@@ -83,34 +81,7 @@ public class DJuQBox implements EntryPoint {
 		nameField.setFocus(true);
 		nameField.selectAll();
 
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
-
+		
 		// Create a handler for the sendButton and nameField
 		class MyHandler implements ClickHandler, KeyUpHandler /* , MethodCallback */{
 			/**
@@ -118,192 +89,15 @@ public class DJuQBox implements EntryPoint {
 			 */
 			public void onClick(ClickEvent event) {
 
-				// test Resty
-				testResty();
-
-				// testRestyIntf();
-
-				// sendNameToServer();
+				GWT.log(event.getSource().toString());
 
 			}
 
-			// private void testRestyIntf() {
-			// RestApiInterface api = GWT.create(RestApiInterface.class);
-			// REST.withCallback(null).call(api).getRooms();
-			// }
-			//
-			// @Override
-			// public void onFailure(Method method, Throwable ex) {
-			// // TODO Auto-generated method stub
-			// Window.alert("error: " + ex.getMessage());
-			// }
-			//
-			// @Override
-			// public void onSuccess(Method method, Object response) {
-			//
-			// // mallon den ginetai, de mou dinei kati to method gia na tis
-			// // jexwrisw.ektos an mprow na parw kapws to url?
-			// Window.alert("OK " + method);
-			//
-			// }
-
-			// test conflicts 2
-			// test branch acs
-			// paizei wraia ftanei na kaneis Fetch, Pull kai meta Push (sou
-			// bgazei oti de mporei na gine push non-fast-forward) kai kanei
-			// automatata merge kai sta idia ta files
-
-			private void testResty() {
-				RestApiService api = GWT.create(RestApiService.class);
-				
-				
-				
-				api.getRoom("testId", new MethodCallback<Room>() {
-					
-					@Override
-					public void onSuccess(Method method, Room response) {
-						// TODO Auto-generated method stub
-						Window.alert("OK3 method  "  + method.builder.getUrl() );
-						Window.alert("OK3 name  "  + response.getName()  );
-						Window.alert("OK3 name  "  + response.getDate1().toString()  );
-					}
-					
-					@Override
-					public void onFailure(Method method, Throwable exception) {
-						// TODO Auto-generated method stub
-						Window.alert("error getRoom: " + exception.getMessage());
-					}
-				});
-
-				api.getRoomUsers(new Room("test55 "), new MethodCallback<List<User>>() {
-
-					@Override
-					public void onSuccess(Method method, List<User> response) {
-
-						Window.alert("OK " + response.get(0).getName());
-
-					}
-
-					@Override
-					public void onFailure(Method method, Throwable exception) {
-						// TODO Auto-generated method stub
-						Window.alert("error: " + exception.getMessage());
-					}
-				});
-
-				
-				
-				api.addUserToRoom("aRoomId", new User("1234"), new MethodCallback<Void>() {
-					
-					@Override
-					public void onSuccess(Method method, Void response) {
-						// TODO Auto-generated method stub
-						Window.alert("OK2 Void "  + method.builder.getUser() );
-					}
-					
-					@Override
-					public void onFailure(Method method, Throwable exception) {
-						// TODO Auto-generated method stub
-						Window.alert("error2: " + exception.getMessage());
-					}
-				});
-
-				
-				
-//				api.addUserToRoom("aRoomId", new User("1234"), new MethodCallback<Void>() {
-//
-//					@Override
-//					public void onSuccess(Method method) {
-//
-//						Window.alert("OK2 ");// + response.getName());
-//
-//					}
-//
-//					@Override
-//					public void onFailure(Method method, Throwable exception) {
-//						Window.alert("error2: " + exception.getMessage());
-//
-//					}
-//				});
-
-				// api.getRooms(new MethodCallback<List<Room>>() {
-				//
-				// @Override
-				// public void onSuccess(Method method, List<Room> response) {
-				// // all ok
-				// Window.alert("OK " + response.get(0).getId());
-				// //test coflict
-				// }
-				//
-				// @Override
-				// public void onFailure(Method method, Throwable ex) {
-				// // error
-				// Window.alert("error: " + ex.getMessage());
-				//
-				// }
-				// });
-				//
-				// api.getRoomUsers(new Room("restroom"), new
-				// MethodCallback<List<User>>() {
-				//
-				// @Override
-				// public void onSuccess(Method method, List<User> response) {
-				// Window.alert("OK " + response.get(0).getId());
-				// }
-				//
-				// @Override
-				// public void onFailure(Method method, Throwable ex) {
-				// Window.alert("error: " + ex.getMessage());
-				// }
-				// });
-
-			}
-
-			/**
-			 * Fired when the user types in the nameField.
-			 */
+			@Override
 			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
-			}
-
-			/**
-			 * Send the name from the nameField to the server and wait for a
-			 * response.
-			 */
-			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
-				String textToServer = nameField.getText();
-				if (!FieldVerifier.isValidName(textToServer)) {
-					errorLabel.setText("Please enter at least four characters");
-					return;
-				}
-
-				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer, new AsyncCallback<String>() {
-					public void onFailure(Throwable caught) {
-						// Show the RPC error message to the user
-						dialogBox.setText("Remote Procedure Call - Failure");
-						serverResponseLabel.addStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML(SERVER_ERROR);
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
-
-					public void onSuccess(String result) {
-						dialogBox.setText("Remote Procedure Call");
-						serverResponseLabel.removeStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML(result);
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
-				});
-			}
+			
+				
+			}			
 
 		}
 
