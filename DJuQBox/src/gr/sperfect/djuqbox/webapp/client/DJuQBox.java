224caa6ec6505fdec04e889b5f8d4fd13347fa8f
sperfect(ACS) <sperfect@gmail.com>
2015-03-06 13:40:45 +0200

    test mqtt OK

--- DJuQBox/src/gr/sperfect/djuqbox/webapp/client/DJuQBox.java
+++ DJuQBox/src/gr/sperfect/djuqbox/webapp/client/DJuQBox.java
@@ -31,6 +31,15 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
+import com.jooink.experiments.mqtt.Client;
+import com.jooink.experiments.mqtt.ConnectionLostEvent;
+import com.jooink.experiments.mqtt.Destination;
+import com.jooink.experiments.mqtt.MessageArrivedEvent;
+import com.jooink.experiments.mqtt.Subscription;
+import com.jooink.experiments.mqtt.ConnectionLostEvent.Handler;
+import com.jooink.experiments.mqtt.lowlevel.ConnectionHandler;
+import com.jooink.experiments.mqtt.lowlevel.MqttMessage;
+import com.jooink.experiments.mqtt.lowlevel.SubscriptionHandler;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -44,7 +53,7 @@ public class DJuQBox implements EntryPoint {
 		// "Could not parse response: org.fusesource.restygwt.client.ResponseFormatException: Response was NOT a valid JSON document"
 		Defaults.setDateFormat(null);
 	}
-	
+
 	public static final RestApiService API = GWT.create(RestApiService.class);
 
 	/**
@@ -158,36 +167,36 @@ public class DJuQBox implements EntryPoint {
 					}
 				});
 
-//				api.createUser(new User("test user"), new MethodCallback<User>() {
-//
-//					@Override
-//					public void onSuccess(Method method, User uRes) {
-//
-//						Window.alert("OK " + uRes.getUID());
-//
-//						api.getUser(uRes.getUID(), new MethodCallback<User>() {
-//
-//							@Override
-//							public void onSuccess(Method method, User u) {
-//								Window.alert("OK2 " + u.getUID());
-//
-//							}
-//
-//							@Override
-//							public void onFailure(Method method, Throwable exception) {
-//								Log("getUser ", method, exception);
-//
-//							}
-//						});
-//
-//					}
-//
-//					@Override
-//					public void onFailure(Method method, Throwable exception) {
-//						Log("createUser ", method, exception);
-//
-//					}
-//				});
+				// api.createUser(new User("test user"), new MethodCallback<User>() {
+				//
+				// @Override
+				// public void onSuccess(Method method, User uRes) {
+				//
+				// Window.alert("OK " + uRes.getUID());
+				//
+				// api.getUser(uRes.getUID(), new MethodCallback<User>() {
+				//
+				// @Override
+				// public void onSuccess(Method method, User u) {
+				// Window.alert("OK2 " + u.getUID());
+				//
+				// }
+				//
+				// @Override
+				// public void onFailure(Method method, Throwable exception) {
+				// Log("getUser ", method, exception);
+				//
+				// }
+				// });
+				//
+				// }
+				//
+				// @Override
+				// public void onFailure(Method method, Throwable exception) {
+				// Log("createUser ", method, exception);
+				//
+				// }
+				// });
 
 				// api.getYoutubeMixForSong(searchField.getText(), new
 				// MethodCallback<YoutubePlayList>() {
@@ -233,21 +242,16 @@ public class DJuQBox implements EntryPoint {
 		}
 
 		RootPanel.get("roomContainer").add(currentRoom);
-		
-		
 
-		
 		// Add a handler to send the name to the server
 		MyHandler handler = new MyHandler();
 		sendButton.addClickHandler(handler);
 		searchField.addKeyUpHandler(handler);
 
-		
-
 		DOM.getElementById("loading").removeFromParent();
 
-		//PlayerControls pc = new PlayerControls(controlHandler);
-		//RootPanel.get("playerControlsContainer").add(pc);
+		// PlayerControls pc = new PlayerControls(controlHandler);
+		// RootPanel.get("playerControlsContainer").add(pc);
 
 		// Timer t = new Timer() {
 		//
@@ -260,22 +264,113 @@ public class DJuQBox implements EntryPoint {
 		// };
 		//
 		// t.scheduleRepeating(5000);
-		
+
 		GWT.log("before getCurrentRoom");
 		getCurrentRoom();
-		
+
 		GWT.log("after getCurrentRoom");
+
+		testMqtt();
+
+		// pairnei 2?
+		sendButton.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+
+				MqttMessage m = MqttMessage.create(searchField.getText());
+				m.setDestinationName("testtt");
+				m.setQos(0);
+				m.setRetained(false);
+
+				client.send(m);
+
+			}
+		});
 
 	}
 
-	final RoomWidget currentRoom = new RoomWidget();
-
+	static int counter = 0;
 	
+	private void testMqtt() {
+
+		counter = ((int) (Math.random() *100));
+		client  = new Client("test.mosquitto.org", 8080, "testClientId" + counter );
+		
+		client.addConnectionLostHandler(new Handler() {
+
+			@Override
+			public void onConnectionLost(ConnectionLostEvent e) {
+				// TODO Auto-generated method stub
+				Window.alert("onConnectionLost");
+			}
+		});
+
+		final MessageArrivedEvent.Handler myh = new MessageArrivedEvent.Handler() {
+
+			@Override
+			public void onMessageArrived(MessageArrivedEvent e) {
+				// TODO Auto-generated method stub
+				Window.alert("2onMessageArrived" + e.getMessage().getDestinationName());
+			}
+
+		};
+
+		ConnectionHandler ca = new ConnectionHandler() {
+
+			@Override
+			public void onSuccess() {
+				// update the header status
+				Window.alert("onSuccess");
+				// can go ... it is connected
+
+				Destination topic = new Destination("testtt");
+				final Subscription subscription = new Subscription(client, topic);
+
+				SubscriptionHandler sh = new SubscriptionHandler() {
+
+					@Override
+					public void onSubscriptionSuccess() {
+						// TODO Auto-generated method stub
+						Window.alert("onSubscriptionSuccess");
+
+						// subscription.addMessageArrivedHandler(myh);
+						// oxi edw giati 8a xasw to retained
+					}
+
+					@Override
+					public void onSubscriptionFailure(int errorCode, String errorText) {
+						// TODO Auto-generated method stub
+						Window.alert("onSubscriptionFailure " + errorText);
+					}
+				};
+
+				Window.alert("wait sub ");
+
+				subscription.addMessageArrivedHandler(myh);
+				subscription.subscribe(sh);
+			}
+
+			@Override
+			public void onFailure(int errorCode, String errorText) {
+				// update the header status
+				Window.alert("onFailure " + errorText);
+
+			}
+		};
+
+		client.connect(ca, "", "", 60);
+
+	}
+	
+	Client client ;//=
+
+	final RoomWidget currentRoom = new RoomWidget();
 
 	protected void getCurrentRoom() {
 
 		API.getRoomByValue("name", "demoRoom", new MethodCallback<Room>() {
-			
+
 			@Override
 			public void onSuccess(Method method, Room r) {
 				// TODO Auto-generated method stub
@@ -283,7 +378,7 @@ public class DJuQBox implements EntryPoint {
 				currentRoom.setRoom(r);
 				currentRoom.load(r);
 			}
-			
+
 			@Override
 			public void onFailure(Method method, Throwable exception) {
 				// TODO Auto-generated method stub
@@ -291,7 +386,6 @@ public class DJuQBox implements EntryPoint {
 			}
 		});
 
-	
 	}
 
 	private void Log(String message, Method method, Throwable ex) {
