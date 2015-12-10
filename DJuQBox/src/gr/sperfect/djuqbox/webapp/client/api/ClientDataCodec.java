6871a86e9ccebf63a442625eda413f50050e41a3
sperfect(ACS) <sperfect@gmail.com>
2015-12-10 17:07:54 +0200

    ClientDataCodec for jsoning

---
+++ DJuQBox/src/gr/sperfect/djuqbox/webapp/client/api/ClientDataCodec.java
@@ -0,0 +1,9 @@
+package gr.sperfect.djuqbox.webapp.client.api;
+
+import gr.sperfect.djuqbox.webapp.shared.data.Client;
+
+import org.fusesource.restygwt.client.JsonEncoderDecoder;
+
+public interface ClientDataCodec extends JsonEncoderDecoder<Client> {
+
+}
